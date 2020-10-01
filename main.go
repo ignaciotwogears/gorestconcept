346f2486828109b3e5bf854e06a8bca9a23b19e1
ignaciotwogears <56442904+ignaciotwogears@users.noreply.github.com>
2020-09-30 21:15:12 -0300

    structure set up

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+//https://levelup.gitconnected.com/crud-restful-api-with-go-gorm-jwt-postgres-mysql-and-testing-460a85ab7121
+import "github.com/ignaciotwogears/gorestconcept/api"
+
+func main() {
+	api.Run()
+}
